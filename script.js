2d12db21788cedda02ab185464c5313313a1aa4d
Aarav Gupta <agupta-22@peddie.org>
2022-01-20 19:41:16 -0500

    menu feed done

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ fetch("/schedule.txt")
     })
     .then(text => document.getElementById("portfolio-code").innerText = text)
     .catch(errror => document.getElementById("portfolio-code").innerText = "Unable to fetch portfolio, try again later")
+
+  fetch("/menu.txt")
+    .then(response => {
+      if(!response.ok) {
+        throw new Error("Bad Response")
+      }
+      return response.text()
+    })
+    .then(text => document.getElementById("portfolio-code2").innerText = text)
+    .catch(errror => document.getElementById("portfolio-code2").innerText = "Unable to fetch portfolio, try again later")
+
+   
