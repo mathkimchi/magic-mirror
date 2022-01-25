87e26daf09c3c687d52c7cf4b121a44ea296f47f
Aarav Gupta <agupta-22@peddie.org>
2022-01-25 10:31:29 -0500

    update

--- node.js
+++
@@ -1,3 +0,0 @@
-fetch('C:/Users/agupta-22/Magic-Mirror/games2.txt')
-.then(response => response.text())
-.then(text => console.log(text))
--- schedule.py
+++ schedule.py
@@ -1,94 +1,94 @@
-from datetime import datetime
 from bs4 import BeautifulSoup
-
-weekday=datetime.today().strftime('%A')
+from datetime import datetime
 
 file = open("C:/Users/agupta-22/Magic-Mirror/schedule.txt", "w")
+weekday=datetime.today().strftime('%A')
 
 time = datetime.now().strftime("%H:%M")
-
 print(time)
 file.write("Current Block: ")
 if(time >= "8:00" and time <="8:50" and weekday == "Monday"):
   file.write("A")
 elif(time >= "9:00" and time <="10:15" and weekday == "Monday"):
-    file.write("B")
+     file.write("B")
 elif(time >= "10:25" and time <="11:25" and weekday == "Monday"):
      file.write("C")
 elif(time >= "11:25" and time <="12:15"and weekday == "Monday"):
-    file.write("Chapel")
+     file.write("Chapel")
 elif(time > "12:15" and time <="12:55" and weekday == "Monday"):
      file.write("Lunch")
 elif(time > "12:55" and time <="14:10" and weekday == "Monday"):
-   file.write("D")
+     file.write("D")
 elif(time >= "14:20" and time <="15:10" and weekday == "Monday"):
      file.write("E")
 
 if(time >= "8:00" and time <="8:50" and weekday == "Tuesday"):
-  file.write("F")
+     file.write("F")
 elif(time >= "9:00" and time <="10:15" and weekday == "Tuesday"):
-    file.write("G")
+     file.write("G")
+    
 elif(time >= "10:25" and time <="11:25" and weekday == "Tuesday"):
      file.write("A")
 elif(time >= "11:25" and time <="12:15"and weekday == "Tuesday"):
-    file.write("DMX")
+     file.write("DMX")
 elif(time > "12:15" and time <="13:20" and weekday == "Tuesday"):
      file.write("Lunch")
 elif(time > "13:20" and time <="14:10" and weekday == "Tuesday"):
-   file.write("C")
+     file.write("C")
 elif(time >= "14:20" and time <="15:10" and weekday == "Tuesday"):
      file.write("B")
 
 if(time >= "8:00" and time <="8:50" and weekday == "Wednesday"):
-  file.write("D")
+     file.write("D")
 elif(time >= "9:00" and time <="10:15" and weekday == "Wednesday"):
-    file.write("E")
+     file.write("E")
 elif(time >= "10:25" and time <="11:25" and weekday == "Wednesday"):
      file.write("G")
 elif(time >= "11:25" and time <="12:15"and weekday == "Wednesday"):
-    file.write("Community Meeting")
+     file.write("Community Meeting")
 elif(time > "12:15" and time <="12:55" and weekday == "Wednesday"):
      file.write("Lunch")
 elif(time > "12:55" and time <="13:45" and weekday == "Wednesday"):
-   file.write("F")
+     file.write("F")
 
 if(time >= "8:00" and time <="8:50" and weekday == "Thursday"):
-  file.write("B")
+     file.write("B")
 elif(time >= "9:00" and time <="10:15" and weekday == "Thursday"):
-    file.write("C")
+     file.write("C")
 elif(time >= "10:25" and time <="11:15" and weekday == "Thursday"):
      file.write("D")
 elif(time >= "11:25" and time <="12:15"and weekday == "Thursday"):
-    file.write("DMX")
+     file.write("DMX")
 elif(time > "12:15" and time <="13:20" and weekday == "Thursday"):
      file.write("Lunch")
 elif(time > "13:20" and time <="14:10" and weekday == "Thursday"):
-   file.write("E")
+     file.write("E")
 elif(time >= "14:20" and time <="15:10" and weekday == "Thursday"):
      file.write("A")
 
 if(time >= "8:00" and time <="8:50" and weekday == "Friday"):
-  file.write("G")
+     file.write("G")
 elif(time >= "9:00" and time <="10:15" and weekday == "Friday"):
-    file.write("F")
+     file.write("F")
 elif(time >= "10:25" and time <="11:25" and weekday == "Friday"):
      file.write("B")
 elif(time >= "11:25" and time <="12:15"and weekday == "Friday"):
-    file.write("Chapel")
+     file.write("Chapel")
 elif(time > "12:15" and time <="12:55" and weekday == "Friday"):
      file.write("Lunch")
 elif(time > "12:55" and time <="14:10" and weekday == "Friday"):
-   file.write("A")
+     file.write("A")
 elif(time >= "14:20" and time <="15:10" and weekday == "Friday"):
      file.write("C")
 
 
 if(time >= "8:00" and time <="8:50" and weekday == "Saturday"):
-  file.write("E")
+     file.write("E")
 elif(time >= "9:00" and time <="9:50" and weekday == "Saturday"):
-    file.write("D")
+     file.write("D")
 elif(time >= "10:00" and time <="10:50" and weekday == "Saturday"):
      file.write("F")
 elif(time >= "11:00" and time <="11:50"and weekday == "Saturday"):
-    file.write("G")
+     file.write("G")
+
 file.close()
