e2b39663923e00d7d1ed2e8b02e25775468e9c93
Aarav Gupta <agupta-22@peddie.org>
2022-01-25 20:13:41 -0500

    calendar feed dates working

--- calendar1.py
+++ calendar1.py
@@ -2,12 +2,22 @@ from icalendar import Calendar, Event
 from datetime import datetime
 from pytz import UTC # timezone
 from datetime import date
+from datetime import datetime
+import datetime
+from dateutil import relativedelta
 # Returns the current local date
 todayDate=date.today()
 todayMonth=str(todayDate)
 todayMonth=todayMonth[5:7]
 todayDay=str(todayDate)
 todayDay=todayDay[8:]
+nextMonth = datetime.date.today() + relativedelta.relativedelta(months=1)
+nextMonth2=str(nextMonth)
+nextMonth2=nextMonth2[5:7]
+nextDay=str(nextMonth)
+nextDay=nextDay[8:]
+todayMonth2=todayDate.strftime("%B")
+nextMonth3=nextMonth.strftime("%B")
 file1 = open('C:/Users/agupta-22/Magic-Mirror/calendar1.txt', 'w')
 def findCalendar(textFile) :
     g = open(textFile,'rb')
@@ -18,26 +28,33 @@ def findCalendar(textFile) :
             startTimeString = startTime.decode('utf-8')
             startTimeString1=startTimeString[4:6]
             startTimeString2=startTimeString[6:8]
-            if startTimeString1 == todayMonth and startTimeString2 >= todayDay:
+            
+            if startTimeString1 == todayMonth and startTimeString2 >= nextDay or startTimeString1 == nextMonth2:
                 menu=component.get('summary')
                 menu2=component.get('location') 
 
                 
                 if menu != None and menu2 != None:
                     file1.write(menu)
+               
                     file1.write('\n')
                     
                     file1.write(menu2)
                     file1.write('\n')
                 elif menu!= None:
                     file1.write(menu)
+                    if startTimeString1 == todayMonth:
+                         file1.write('\t'+todayMonth2)
+                    elif startTimeString1== nextMonth2:
+                         file1.write('\t'+nextMonth3)
+                    file1.write('\t'+startTimeString2)
                     file1.write('\n')
                     file1.write('')
             else:
                 menu=''
                 file1.write(menu)
+            
          
     g.close()
 findCalendar('calendar1.ics')   
-findCalendar('team_155_gmt.ics')
 file1.close()
