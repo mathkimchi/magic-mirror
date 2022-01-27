fb7215a5f00e765820e9a4ee0b0b45185c15ddd7
Aarav Gupta <agupta-22@peddie.org>
2022-01-27 08:12:03 -0500

    today

--- counter.py
+++ counter.py
@@ -5,7 +5,7 @@ file = open("C:/Users/agupta-22/Magic-Mirror/counter.txt", "w")
 weekday = datetime.today().strftime("%A")
 
 time = datetime.now().strftime("%H:%M")
-count= 123
+count= 122
 if time == "24:00":
  count2=count-1
  count2 = str(count2)
