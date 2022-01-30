7794e0d0bce87a1f277080683a49658d4b194ee7
Aarav Gupta <agupta-22@peddie.org>
2022-01-29 19:52:22 -0500

    athletics schedule being fixed

---
+++ athletics.py
@@ -0,0 +1,115 @@
+from icalendar import Calendar, Event
+from datetime import datetime
+from pytz import UTC  # timezone
+from datetime import date
+from datetime import datetime
+import datetime
+from dateutil import relativedelta
+
+# Returns the current local date
+todayDate = date.today() 
+todayMonth = str(todayDate)
+todayMonth = todayMonth[5:7]
+todayDay = str(todayDate)
+todayDay = todayDay[8:]
+nextMonth = datetime.date.today() + relativedelta.relativedelta(months=1) 
+nextMonth2 = str(nextMonth)
+nextMonth2 = nextMonth2[5:7]
+nextDay = str(nextMonth)
+nextDay = nextDay[8:]
+actualDay = date.today().day - 20
+actualDay = str(actualDay)
+todayMonth2 = todayDate.strftime("%B")
+nextMonth3 = nextMonth.strftime("%B")
+file1 = open("C:/Users/agupta-22/Magic-Mirror/athletics.txt", "w")
+
+
+def findCalendar(textFile):
+    g = open(textFile, "rb")
+    gcal = Calendar.from_ical(g.read())
+    for component in gcal.walk():
+        if component.name == "VEVENT":
+            startTime = component.get("DTSTART").to_ical()
+            startTimeString = startTime.decode("utf-8")
+            startTimeString1 = startTimeString[4:6]
+            startTimeString2 = startTimeString[6:8]
+            if (
+                startTimeString1 == todayMonth
+                and startTimeString2 >= nextDay
+                or startTimeString1 == nextMonth2
+                and startTimeString2 <= actualDay
+            ):
+                menu = component.get("summary")
+                menu2 = component.get("location")
+                if menu.find("Away")>0:
+                    file1.write(menu)
+                    if menu != None and menu2 != None:
+                        file1.write("")
+                    elif menu != None:
+                        file1.write(menu + " - ")
+                    if startTimeString1 == todayMonth:
+                        file1.write("\t" + todayMonth2)
+                    elif startTimeString1 == nextMonth2:
+                        file1.write("\t" + nextMonth3)
+                        file1.write("\t" + startTimeString2)
+                        file1.write("\n")
+                        file1.write("")
+                    file1.write("\n")   
+                elif menu.find("Home")>0:
+                    file1.write(menu)  
+                    if menu != None and menu2 != None:
+                        file1.write("")
+                    elif menu != None:
+                        file1.write(menu + " - ")
+                    if startTimeString1 == todayMonth:
+                        file1.write("\t" + todayMonth2)
+                    elif startTimeString1 == nextMonth2:
+                        file1.write("\t" + nextMonth3)
+                        file1.write("\t" + startTimeString2)
+                        file1.write("\n")
+                        file1.write("")
+                    file1.write("\n")     
+                else:
+                   menu = ""
+                   file1.write(menu)
+
+    g.close()
+
+findCalendar("team_155_gmt.ics")
+findCalendar("team_156_gmt.ics")
+findCalendar("team_157_gmt.ics")
+findCalendar("team_158_gmt.ics")
+findCalendar("team_159_gmt.ics")
+findCalendar("team_160_gmt.ics")
+findCalendar("team_161_gmt.ics")
+findCalendar("team_162_gmt.ics")
+findCalendar("team_163_gmt.ics")
+findCalendar("team_164_gmt.ics")
+findCalendar("team_165_gmt.ics")
+findCalendar("team_166_gmt.ics")
+findCalendar("team_167_gmt.ics")
+findCalendar("team_168_gmt.ics")
+findCalendar("team_169_gmt.ics")
+findCalendar("team_171_gmt.ics")
+findCalendar("team_172_gmt.ics")
+findCalendar("team_173_gmt.ics")
+findCalendar("team_174_gmt.ics")
+findCalendar("team_175_gmt.ics")
+findCalendar("team_176_gmt.ics")
+findCalendar("team_177_gmt.ics")
+findCalendar("team_178_gmt.ics")
+findCalendar("team_180_gmt.ics")
+findCalendar("team_181_gmt.ics")
+findCalendar("team_182_gmt.ics")
+findCalendar("team_183_gmt.ics")
+findCalendar("team_184_gmt.ics")
+findCalendar("team_185_gmt.ics")
+findCalendar("team_186_gmt.ics")
+findCalendar("team_187_gmt.ics")
+findCalendar("team_189_gmt.ics")
+findCalendar("team_190_gmt.ics")
+findCalendar("team_191_gmt.ics")
+findCalendar("team_192_gmt.ics")
+findCalendar("team_194_gmt.ics")
+file1.close()
+
