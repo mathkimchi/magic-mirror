0f09d998311e801ef56711deaabed1d257fceed0
Aarav Gupta <agupta-22@peddie.org>
2022-01-30 13:37:49 -0500

    Pictures of the Week Section Added

--- athletics4.py
+++ athletics4.py
@@ -2,7 +2,7 @@ file1 = open("C:/Users/agupta-22/Magic-Mirror/athletics4.txt", "w")
 with open('athletics3.txt', 'r') as f:
     cnt = 1
     for line in f:
-        if cnt <= 18:
+        if cnt <= 16:
             print(line, end='', file=open("athletics4.txt", "a"))
             cnt += 1
         else:
