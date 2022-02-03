2106edb5eaba8e7d563ebd2275d7863a8dbc7c3c
Aarav Gupta <agupta-22@peddie.org>
2022-02-03 18:01:34 -0500

    switched calnedar and athletics feed

--- calendar2.py
+++ calendar2.py
@@ -2,7 +2,7 @@ file1 = open("C:/Users/agupta-22/Magic-Mirror/calendar2.txt", "w")
 with open('calendar1.txt', 'r') as f:
     cnt = 1
     for line in f:
-        if cnt <= 17:
+        if cnt <= 16:
             print(line, end='', file=open("calendar2.txt", "a"))
             cnt += 1
         else:
