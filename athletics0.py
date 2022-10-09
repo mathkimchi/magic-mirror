4fb2dfe28c0474fa2129c38a9d508c86601435d6
Tomaz Chevres <tchevres-24@peddie.org>
2022-10-08 22:58:18 -0400

    Fixed athletics0.py, saves to the correct folder now

--- athletics0.py
+++ athletics0.py
@@ -4,6 +4,6 @@ import requests
 for x in range(154,195):
     url = 'https://www.peddie.org/calendar/team_'+str(x)+'.ics' #get url name
     r = requests.get(url, allow_redirects=True)                 #get file at url
-    open('Calendars\\team_'+str(x)+'_gmt.ics', 'wb').write(r.content)      #save file
+    open('Calendars/team_'+str(x)+'_gmt.ics', 'wb').write(r.content)      #save file
 
 print(__file__) #print after program is run (for debugging in webmin)
