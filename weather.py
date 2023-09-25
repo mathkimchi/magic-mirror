3bc5d7490ca8ae41b74694c2b7d3d86cb09ad0bb
PeddieCompSciClub <compsciclub@peddie.org>
2023-09-25 09:57:04 -0400

    Reformat grill2 (based on blair-day)

--- weather.py
+++ weather.py
@@ -1,8 +1,9 @@
+from encodings import utf_8
 from bs4 import BeautifulSoup as bs
 import requests
 
 # https://www.thepythoncode.com/article/extract-weather-data-python
-file = open("weather.txt", "w")
+file = open("weather.txt", "w", encoding="utf-8")
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"
 # US english
 LANGUAGE = "en-US,en;q=0.5"
@@ -20,6 +21,9 @@ def get_weather_data(url):
     result = {}
     result['weather_now'] = soup.find("span", attrs={"id": "wob_dc"}).text
     result["temp_now"] = soup.find("span", attrs={"id": "wob_tm"}).text
+    result["weather_img"] = "https:" + soup.find("img", attrs={"id": "wob_tci"})["src"]
+    result["humidity"] = soup.find("span", attrs={"id": "wob_hm"}).text
+    result["wind"] = soup.find("span", attrs={"id": "wob_ws"}).text
     return result
 
 
@@ -43,5 +47,13 @@ if __name__ == "__main__":
     URL += region
     # get data
     data = get_weather_data(URL)
-    file.write(f"Currently: {data['weather_now']}\n")
-    file.write(f"Temperature now: {data['temp_now']}")
+    file.write(f"{data['temp_now']}\u2109\n")
+    file.write(f"{data['weather_now']}\n")
+    file.write(f"Humidity: {data['humidity']}\n")
+    file.write(f"Wind: {data['wind']}")
+    open('weather.png', 'wb').write(requests.get(data["weather_img"], allow_redirects=True).content)
+
+    file.close()
+
+#debugging:
+print(__file__)
