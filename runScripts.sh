c0a9a4a3c8d92ce78013265902efc82b8c0ab509
Tomaz Chevres <tchevres-24@peddie.org>
2023-10-23 09:54:38 -0400

    add runScripts.sh

--- runScripts.sh
+++ runScripts.sh
@@ -1,2 +1,4 @@
-cd
+# cd
 pwd
+
+#cd; cd /var/www/CSProjects/live/Magic-Mirror;python3 athletics0.py;python3 athletics.py; python3 athletics2.py; python3 athletics3.py; python3 athletics4.py; python3 calendar1.py; python3 calendar2.py; python3 menu.py; python3 menu2.py; python3 weather.py;
